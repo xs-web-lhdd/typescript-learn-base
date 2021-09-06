fb3752c81d2ce30930450f9edb8ba2091f163bbf
凉风有信 <3042146237@qq.com>
2021-09-06 11:31:21 +0800

    feat:使用cheerio进行数据提取

--- crawler/src/crowller.ts
+++ crawler/src/crowller.ts
@@ -1,5 +1,11 @@
 // 可在node发送请求
 import superagent from 'superagent' // 记得安装类型定义文件依赖
+import cheerio from 'cheerio'
+
+interface Msg {
+  title: string,
+  imgUrl: string | undefined
+}
 
 class Crowller {
   private url = 'http://www.dell-lee.com/'
@@ -8,7 +14,27 @@ class Crowller {
 
   async getRawHtml() {
     const result = await superagent.get(this.url)
-    console.log(result.text);
+    const htmlString = result.text
+    this.getJsonInfo(htmlString)
+  }
+
+  getJsonInfo(html: string) {
+    let MsgArr: Msg[] = []
+    const $ = cheerio.load(html);
+    const CourseItems = $('.course-item')
+    // 将想爬取的 title imgUrl 择出来
+    CourseItems.map((index, element) => {
+      const descs = $(element).find('.course-desc')
+      const imgs = $(element).find('.course-img')
+      const title = descs.text()
+      const imgUrl = imgs.eq(0).attr("src")
+      MsgArr.push({title,  imgUrl})
+    })
+    const result = {
+      time: new Date().getTime(),
+      data: MsgArr
+    }
+    console.log(result);
     
   }
 
