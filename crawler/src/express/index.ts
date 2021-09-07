af027d458455fb773aa9e439296ea185ecad40d2
凉风有信 <3042146237@qq.com>
2021-09-07 15:04:32 +0800

    feat:express页面展示

--- crawler/src/express/index.ts
+++ crawler/src/express/index.ts
@@ -1,8 +1,17 @@
-import express, { Request, Response } from 'express'
+import express, { Request, Response, NextFunction } from 'express'
+import bodyParser from 'body-parser';
 import router from './router';
+import cookieSession from 'cookie-session'
 
 const app = express()
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieSession({
+  name: 'session',
+  keys: ['express'],
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
 app.use(router)
 
 app.listen(7001, () => {
--- crawler/src/express/router.ts
+++ crawler/src/express/router.ts
@@ -6,6 +6,16 @@
 import { Router, Request, Response } from 'express'
 import DellCrowller from '../单例模式/DellCrowller'
 import { DellAnalyzer } from '../单例模式/DellAnalyzer'
+import path from 'path'
+import fs from 'fs'
+
+
+interface RequestWithBody extends Request {
+  body: {
+    password: string | undefined,
+    [key: string]: string | undefined
+  }
+}
 
 const router = Router()
 
@@ -13,9 +23,79 @@ const url = 'http://www.dell-lee.com/'
 const analyzer = DellAnalyzer.getInstance()
 
 router.get('/', (req: Request, res: Response) => {
-  new DellCrowller(analyzer, url)
-  
-  res.send('getData Success')
+  const isLogin = req.session ? req.session.login : false
+  if (isLogin) {
+    res.send(`
+    <html>
+      <body>
+        <a href='/getData'>爬取内容</a>
+        <a href='/showData'>展示内容</a>
+        <a href='/logout'>退出</a>
+      </body>
+    </html>
+    `)
+  } else {
+    res.send(`
+    <html>
+      <body>
+        <form method="post" action="/login">
+          <input type="password" name="password" />
+          <button>登录</button>
+        </form>
+      </body>
+    </html>
+  `)
+  }
+})
+
+router.get('/getData', (req: RequestWithBody, res: Response) => {
+  const isLogin = req.session ? req.session.login : false
+  if (isLogin) {
+    new DellCrowller(analyzer, url)
+    res.send('getData Success')
+    return
+  }
+  res.send(`请登陆后爬取内容！`)
+})
+
+router.post('/login', (req: RequestWithBody, res: Response) => {
+  const { password } = req.body
+  const isLogin = req.session ? req.session.login : false
+
+  if(isLogin) {
+    res.send('你已登录成功！')
+  } else {
+    if (password === '123' && req.session) {
+      if (req.session) {
+        req.session.login = true
+        res.send('登录成功！')
+      }
+    } else {
+      res.send('登陆失败！')
+    }
+  }
+})
+
+router.get('/logout', (req: Request, res: Response) => {
+  if (req.session) {
+    req.session.login = undefined
+  }
+  res.redirect('/')
+})
+
+router.get('/showData', (req: RequestWithBody, res: Response) => {
+  const isLogin = req.session ? req.session.login : false
+  if (isLogin) {
+    try {
+      const position = path.resolve(__dirname, '../../data/DellMsg.json')
+      const result = fs.readFileSync(position, 'utf-8')
+      res.json(JSON.parse(result))
+    } catch (error) {
+      res.send('尚未爬取到内容')
+    }
+  } else {
+    res.send('请先登录。。。')
+  }
 })
 
 export default router
